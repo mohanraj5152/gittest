6806a2814fabd3a6415c0625ac2cbeaa9f3d5076
MOHANRAJ M <15826@TCS.Local>
2019-06-21 16:56:26 +0530

    remove all files

--- app.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var catMe = require('cat-me');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(catMe());
-}).listen(3000);
-
-
-console.log(catMe()); 
